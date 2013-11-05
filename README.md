896c0a701f10ee36acb23c13f5a68235d65a92a1
Jennifer Eliuk <jen7maples@gmail.com>
2013-11-05 11:19:28 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# chrysalis
+
+A Clojure app to document the process of a project of any kind.
+
+## License
+
+Copyright © 2013 Jennifer Eliuk
+
+Distributed under the Eclipse Public License, the same as Clojure.
---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject chrysalis "0.1.0-SNAPSHOT"
+  :description "A Clojure app to document the process of a project of any kind."
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]])
